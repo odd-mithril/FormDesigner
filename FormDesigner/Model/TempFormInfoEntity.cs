a603fa60410cef3dbf42b3bf3d0a2c2a7a17f353
odd-mithril <oddmithril@gmail.com>
2016-11-03 08:46:19 +0800

    16110301完成界面预览，下一步完善界面操作时间

---
+++ FormDesigner/Model/TempFormInfoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FormDesigner
+{
+    [Table("TempFormInfoEntity")]
+    public class TempFormInfoEntity
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string ContentParse { get; set; }
+    }
+}
